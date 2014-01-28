90c85edb87bb8131d75315c4467facc7189682c4
Juanje <juanjemd@hotmail.com>
2014-01-28 11:45:27 +0100

    Agrego bloque

--- GitOtroEjemplo/ViewController.m
+++ GitOtroEjemplo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+typedef void (^MiTipoBloque)(int);
+
 @interface ViewController ()
 
 @end
@@ -18,6 +20,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    MiTipoBloque mibloque;
+    
+    // Creamos una variable numerica con la directiva __block que hace
+    // que podamos modificar su valor desde el interior del bloque
+    // siendo nosotros los responsables de lo que pudiera ocurrir.
+    // En caso de no indicar __block nos saltaría un error en XCode
+    // impidiendo modificar total desde dentro del bloque.
+    __block int total = 0;
+    
+    // Rellenamos el bloque con código
+    mibloque = ^ void (int valor) {
+        total += valor;
+        NSLog(@"Total: %d",total);
+    };
+    
+    // Ejecutamos varias veces el bloque para comprobar
+    mibloque(1);
+    mibloque(1);
+    mibloque(1);
 }
 
 - (void)didReceiveMemoryWarning
